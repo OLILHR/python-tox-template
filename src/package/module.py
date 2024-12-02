6df4563ce4e0ae06c216e1c13f2bf71eb8e32ab1
olli <144932831+OLILHR@users.noreply.github.com>
2024-12-02 20:40:35 +0100

    build: scaffold development environment using tox (#1)
    * Scaffold development environment using tox
    
    * Set python requirement to `3.12`
    
    * Fix workflow dependency installation paths

---
+++ src/package/module.py
@@ -0,0 +1,22 @@
+"""
+Docstring for this module.
+"""
+
+
+# pylint: disable=too-few-public-methods
+class ModuleClass:
+    """
+    Docstring for this class.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize for the sake of initializing.
+        """
+        self.my_instance_var: str = "python-tox-template"
+
+    def function(self) -> str:
+        """
+        Docstring for this function.
+        """
+        return self.my_instance_var
